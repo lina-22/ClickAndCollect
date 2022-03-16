a28c913ab2302d92cdec7de1b3d1613ad6ab0669
lina-22 <haquefahinur@gmail.com>
2022-03-16 23:47:28 +0100

    post route for store method created for CategoryController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/demo',function(){
     return "From Lina's Api";
 });
+
+Route::post('/category',[CategoryController::class, 'store']);
