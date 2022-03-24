a2131e1b2c90e73feaae4dd491194cb7130fb732
lina-22 <haquefahinur@gmail.com>
2022-03-24 07:32:02 +0100

    Api ready for Products table routes/api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
+
 use App\Http\Models\Category;
 
 use App\Http\Controllers\api\CategoryController;
+use App\Http\Controllers\api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,8 +25,17 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/demo',function(){
 //     return "From Lina's Api";
 // });
+
+                       // Routes for categoris
 Route::get('categories', [CategoryController::class, 'showAll']);
 Route::get('categories/{id}', [CategoryController::class, 'showSingle']);
 Route::post('categories', [CategoryController::class, 'store']);
 Route::put('categories/{id}', [CategoryController::class, 'update']);
 Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+
+                        // Routes for products
+Route::get('products', [ProductController::class, 'showAll']);
+Route::get('products/{id}', [ProductController::class, 'showSingle']); 
+Route::post('products', [ProductController::class, 'store']);
+Route::put('products/{id}', [ProductController::class, 'update']);
+Route::delete('products/{id}', [ProductController::class, 'destroy']);
