db782d025fe1cd712431e58ecb96107739ce95c2
lina-22 <haquefahinur@gmail.com>
2022-05-28 05:01:00 +0200

    last modification

--- routes/api.php
+++ routes/api.php
@@ -105,7 +105,7 @@ Route::middleware('auth:sanctum')->group(function () {
     // Route::get('reservations/{id}', [ReservationController::class, 'showSingle']);
     // Route::post('reservations', [ReservationController::class, 'store']);
     // Route::put('reservations/{id}', [ReservationController::class, 'update']);
-    // Route::delete('reservations/{id}', [ReservationController::class, 'destroy']);
+    Route::delete('reservations/{id}', [ReservationController::class, 'destroy']);
 
     // Route for users
     Route::get('users', [UserController::class, 'showAll'])->middleware('admin');
