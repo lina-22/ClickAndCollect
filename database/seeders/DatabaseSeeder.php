57bb75e029371c012153a54809db3ed73952873d
lina-22 <haquefahinur@gmail.com>
2022-06-12 00:23:38 +0200

    some modifications added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +19,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // $category=Category::create(['name'=>'Home', 'is_featured'=>1,'is_featured'=>1, 'image'=>'Category_1653259519.png' ]);
+        // $product=Product::create(['name'=>'wedding dress', 'is_featured'=>1,'price'=>500, 'discount'=>50,'image'=>'product_1653.png', 'description'=>'A weddingpanjabi. It is usually long and marron.']);
+        // $me=User::create([
+        // 'first_name'=>'Lina','last_name'=>'Haque','email'=>'lina@gmail.com', 'password'=>bcrypt('lina@gmail.com1')]);
+
+        // $SuperAdmin=Role::create(['name'=>'SuperAdmin']);
+        // $admin=Role::create(['name'=>'admin']);
+        // $customer=Role::create(['name'=>'customer']);
+
     }
 }
